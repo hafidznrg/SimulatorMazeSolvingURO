1dd4efa244e779a40f6371f1697b326a4ba83cb4
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-07-22 07:44:43 +0700

    pindahin inisiasi objek node ke arena.hpp

--- arena.hpp
+++ arena.hpp
@@ -43,6 +43,23 @@ void Arena::printArena(){
     cout << "|___________________|_____________|" << endl;
 }
 
+// Buat objek node
+Node node1(1, -1, 2, 15, -1, false);
+Node node2(2, -1, 3, -1, 1, false);
+Node node3(3, -1, -1, 4, 2, false);
+Node node4(4, 3, -1, 5, -1, false);
+Node node5(5, 4, -1, -1, 6, false);
+Node node6(6, 7, 5, -1, -1, false);
+Node node7(7, -1, -1, 6, 8, false);
+Node node8(8, 9, 7, -1, 10, true);
+Node node9(9, -1, -1, -1, 8, false);
+Node node10(10, -1, 8, -1, 11, false);
+Node node11(11, 15, 10, 12, -1, false);
+Node node12(12, 11, -1, 13, -1, false);
+Node node13(13, 12, 14, -1, -1, true);
+Node node14(14, -1, -1, -1, 13, false);
+Node node15(15, 1, -1, 11, -1, false);
+
 /**
  * @param robot Objek robot yang udah diconstruct
  * @param number Nomor node yang akan dituju
